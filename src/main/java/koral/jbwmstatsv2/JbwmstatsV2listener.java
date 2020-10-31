6f904e93e651bf25021aa7dd78bcacf4b81c2cea
koral <orszulak.k@o2.pl>
2020-10-31 19:28:45 +0100

    Async support, listener in another class.

---
+++ src/main/java/koral/jbwmstatsv2/JbwmstatsV2listener.java
@@ -0,0 +1,77 @@
+package koral.jbwmstatsv2;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+public class JbwmstatsV2listener implements Listener {
+
+    JbwmstatsV2 plugin;
+    JbwmStatsV2Database database;
+    public JbwmstatsV2listener(final JbwmstatsV2 plugin) {
+        this.plugin = plugin;
+        database = new JbwmStatsV2Database(plugin);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent e){
+        Player p = e.getPlayer();
+        CreatePlayerAsync(p);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent e ) {
+        Player p = e.getPlayer();
+        pushCustomStatsAsync(p);
+    }
+
+    public void pushCustomStatsAsync(Player p) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                database.pushCustomStats(p);
+            }
+        });
+    }
+
+    public void CreatePlayerAsync(Player p) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                database.createPlayerQuery(p);
+            }
+        });
+    }
+
+
+    public void CustomAsyncCreate(String s) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                database.customStatisticCreate(s);
+            }
+        });
+    }
+
+    public void CustomAdvancedStatisticAsyncCreate(String s) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                database.customAdvancedStatisticCreate(s);
+            }
+        });
+    }
+
+    public void ColumnRemoveAsync(String s) {
+        Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+            @Override
+            public void run() {
+                database.statisticRemove(s);
+            }
+        });
+    }
+
+}
